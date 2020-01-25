c9cdcb4ae37713c1ee909b090455de971bfc00ad
crunchypi <alekscrank@gmail.com>
2020-01-25 14:44:19 +0100

    changed row_sep and col_sep defaults

--- packages/cleaning/data_object_tools.py
+++ packages/cleaning/data_object_tools.py
@@ -1,6 +1,6 @@
 # // Some tools related to the DataObj class
 
-def siminet_compressed_to_txt(siminet, row_sep:str = "|rowsep|", col_sep:str = "|colsep|"):
+def siminet_compressed_to_txt(siminet, row_sep:str = "--", col_sep:str = "||"):
     txt = ""
     for row in siminet:
         word = str(row[0])
@@ -8,7 +8,7 @@ def siminet_compressed_to_txt(siminet, row_sep:str = "|rowsep|", col_sep:str = "
         txt += f"{word}{col_sep}{confidence}{row_sep}"
     return txt
 
-def txt_to_compressed_siminet(txt, row_sep:str = "|rowsep|", col_sep:str = "|colsep|"):
+def txt_to_compressed_siminet(txt, row_sep:str = "--", col_sep:str = "||"):
     siminet = []
     rows = txt.split(row_sep)
     for row in rows:
