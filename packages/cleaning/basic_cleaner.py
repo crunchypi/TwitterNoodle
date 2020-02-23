4970d926f0016076e6d61197311fe5380043fa64
crunchypi <alekscrank@gmail.com>
2020-02-23 13:48:07 +0100

    removed unused code

--- packages/cleaning/basic_cleaner.py
+++ packages/cleaning/basic_cleaner.py
@@ -17,65 +17,57 @@ class BasicCleaner():
 
 
     @staticmethod
-    def print_comparison(_data_obj, _text_raw:str) -> None:
+    def print_comparison(data_obj, text_raw:str) -> None:
         try:
             print("##########################")
             print("-------start raw----------")
-            print(_text_raw)
+            print(text_raw)
             print("--------end raw-----------")
             print("-------start new----------")
-            print(_data_obj.text)
-            print(f"sentiment:{_data_obj.valid_sentiment_range}")
-            print(f"hashtags: {_data_obj.hashtags}")
-            print(f"alphatags:{_data_obj.alphatags}")
+            print(data_obj.text)
+            print(f"sentiment:{data_obj.valid_sentiment_range}")
+            print(f"hashtags: {data_obj.hashtags}")
+            print(f"alphatags:{data_obj.alphatags}")
             print("--------end new-----------")
             print("##########################")
         except:
             pass
 
     @classmethod
-    def autocleaner(self, _data_obj, _sentiment_range:float, _verbosity:bool) -> None:
+    def autocleaner(self, data_obj, sentiment_range:float, verbosity:bool) -> None:
 
-        text_raw = _data_obj.text
+        text_raw = data_obj.text
 
-        _data_obj.text = self.clean_links(_data_obj.text)
+        data_obj.text = self.clean_links(data_obj.text)
 
         # // Filter alphatags.
-        filtered_alphatags = self.clean_alphatags(_data_obj.text)
-        _data_obj.text = filtered_alphatags[0]
-        _data_obj.alphatags = filtered_alphatags[1]
+        filtered_alphatags = self.clean_alphatags(data_obj.text)
+        data_obj.text = filtered_alphatags[0]
+        data_obj.alphatags = filtered_alphatags[1]
  
         # // Filter hashtags.
-        filtered_hashtags = self.clean_hashtags(_data_obj.text)
-        _data_obj.text = filtered_hashtags[0]
-        _data_obj.hashtags = filtered_hashtags[1]
+        filtered_hashtags = self.clean_hashtags(data_obj.text)
+        data_obj.text = filtered_hashtags[0]
+        data_obj.hashtags = filtered_hashtags[1]
 
         # // Additional cleaning.
-        _data_obj.text = self.clean_convert_to_lowercase(_data_obj.text)
-        _data_obj.text = self.clean_stopwords(_data_obj.text)
-        _data_obj.text = self.clean_punctuation(_data_obj.text)
-        _data_obj.text = self.remove_duplica_words(_data_obj.text)
-        _data_obj.valid_sentiment_range = self.set_sentiment(
-                                               _data_obj.text,
-                                               _sentiment_range
-                                               )
+        data_obj.text = self.clean_convert_to_lowercase(data_obj.text)
+        data_obj.text = self.clean_stopwords(data_obj.text)
+        data_obj.text = self.clean_punctuation(data_obj.text)
+        data_obj.text = self.remove_duplica_words(data_obj.text)
+        data_obj.valid_sentiment_range = self.set_sentiment(
+                                               data_obj.text,
+                                               sentiment_range
+                                        )
 
-        if _verbosity:
-            self.print_comparison(_data_obj, text_raw)
+        if verbosity:
+            self.print_comparison(data_obj, text_raw)
 
     @staticmethod
     def remove_duplica_words(content:str) -> str:
         words = content.split()
         non_dup = set(words)
         return " ".join(non_dup)
-
-    @staticmethod
-    def tokenise(content):
-        return word_tokenize(content)
-
-    @staticmethod
-    def detokenise(content):
-        return TreebankWordDetokenizer().detokenize(content)
 
     @staticmethod
     def clean_dates(content:str) -> str:
