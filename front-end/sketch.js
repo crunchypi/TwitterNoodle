8c18f28cbc4d4ed26eee7679540f58cda378c4bb
crunchypi <alekscrank@gmail.com>
2020-03-02 21:55:31 +0100

    BROKEN. Adding because of some other changes

--- front-end/sketch.js
+++ front-end/sketch.js
@@ -1,45 +1,44 @@
-let waveHistory = [];
-let time = 0;
-let lerpedLast = 0;
-let lerpGranularity = 0.1;
+let data = []; // # Data from network.
+let dataMultiplier = 1; // # Aplify values.
+let waveHistory = []; // # ColorPoint history.
 
-let data = [];
+let lerpedLast = 0;
+let lerpGranularity = 0.1; // # Smoothness between data values
+
+let dilation = 1; // # Dilates time. Higher val = slower time.
+let colorHeightAdjustment = -50; // # Higher means lower threshold for color.
+
 
 
 function setup() {
   createCanvas(600, 400);
-  startSocket()
+  startSocket();
+  
 }
-
 
 function draw() {
   background(50);
 
-  //let simiVal = stubGetData();
-  let dilated = timeDilation(1, data)
+  // # Do time-dilation (slower/faster value read)
+  //let dilated = timeDilation(dilation, data)
+  let dilated = data.pop();
+  
+  // # Do potential lerping
   if (dilated) {
-    let simiVal = dilated;
-    var lerpedCurrent = lerp(lerpedLast, simiVal, lerpGranularity)
+    var lerpedCurrent = lerp(lerpedLast, dilated, lerpGranularity)
   } else {
     var lerpedCurrent = lerp(lerpedLast, lerpedLast, lerpGranularity)
-  }  
-  renderWave(lerpedCurrent);
-  lerpedLast = lerpedCurrent;
-}
-
-
-
-// # Just get data from index 0
-function stubGetData(){
-  if (data.length > 0) {
-    return data.shift();
-  } else {
-    return 0;
   }
+  lerpedLast = lerpedCurrent;
+  
+  // # Visualise
+  renderWave(lerpedCurrent);
+  
+  
 }
 
 
-function timeDilation(factor, data){ // higher factor -> slower time passed
+function timeDilation(factor, data){ // # higher factor -> slower time passed
   // # Clamp 0 <-> data.length
   if (factor < 0 ) { factor = 1}
   if (factor > data.length) {
@@ -52,42 +51,47 @@ function timeDilation(factor, data){ // higher factor -> slower time passed
   for (let i = 0; i < factor; i++) {
     total += data.shift();
   }
-  return total / factor ;
+  return total / factor;
 }
 
 function renderWave(simiVal){
 
-  // # Create sine.
   translate(0,200);
-  waveHistory.unshift(simiVal);
-  // # Draw
-  beginShape();
-  noFill();
-  stroke(simiVal,0,simiVal)
-  for (let i = 0; i < waveHistory.length; i++) {
-    let xPos = waveHistory.length - i
-    vertex(xPos, waveHistory[i]);
-  }
-  endShape();
   
-  if (waveHistory.length > width / 2) {
+  // # Create new  
+  let colorPoint = new ColorPoint(simiVal);
+  waveHistory.unshift(colorPoint);
+ 
+  // # Draw
+  for (let i = 0; i < waveHistory.length; i++) {
+    // # Adjust xPosition to account for value scaling (time dilation).
+    let xPos = (waveHistory.length - i) / dilation
+    if (i > 0) { // # For the purpose of connecting vertices.
+      beginShape();
+      waveHistory[i].render(xPos, colorHeightAdjustment)
+      // # Connect with last to create shape.
+      waveHistory[i-1].render(xPos, colorHeightAdjustment)
+      endShape()
+    }
+  }
+  // # Clear old.
+  if (waveHistory.length / dilation > width - width/10) {
     waveHistory.pop()
   }
+  // # Debug
+  console.log("datalist: " + data.length + " | wavehistory: " + waveHistory.length)
   
 }
 
-// TODO: just make the color permanent.
-class colorPoint{
+class ColorPoint{
   constructor(value) {
     this.value = value
-    this.position = positionVal
-  }
-  caclColor(val){
-    this.color = (colorVal,colorVal/2 ,colorVal)
-  }
-  render(){
-    vertex(xPos, waveHistory[i]);
+  }  
+  render(xPos, colorAdjustment){
+    stroke(colorAdjustment-this.value, 0, colorAdjustment-this.value)
+    vertex(xPos, this.value);
   }
 }
+
 
 
