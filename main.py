83ed6106efb51264af101ac7eed43fa5191a46f9
crunchypi <alekscrank@gmail.com>
2020-03-02 21:54:28 +0100

    some testing

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from packages.pipes import prefabs
 
-pipeline = prefabs.get_pipeline_api_cln_simi_js(
-    track_api=["to", "and", "from", "but", "how", "i", ],
-    track_incident=["corona", "virus", "outbreak", "death", "sick"]
-)
+# pipeline = prefabs.get_pipeline_api_cln_simi_js(
+#     track_api=["to", "and", "from", "but", "how", "i"],
+#     track_incident=["corona", "virus", "outbreak", "death", "sick"]
+# )
 
-pipeline()
-print("Done.")
+pipeline = prefabs.get_pipeline_api_cln_simi_db(track_api=["to", "and", "from", "but", "how", "i"])
+try:
+    pipeline()
+except KeyboardInterrupt:
+    pass
+print("Done")
