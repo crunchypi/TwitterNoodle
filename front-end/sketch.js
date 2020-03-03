79066665117eb32a07f8aebe0b638fd24e65312a
crunchypi <alekscrank@gmail.com>
2020-03-03 11:18:25 +0100

    fixed

--- front-end/sketch.js
+++ front-end/sketch.js
@@ -1,12 +1,12 @@
 let data = []; // # Data from network.
-let dataMultiplier = 1; // # Aplify values.
+let dataMultiplier = 10; // # Aplify values.
 let waveHistory = []; // # ColorPoint history.
 
 let lerpedLast = 0;
 let lerpGranularity = 0.1; // # Smoothness between data values
 
 let dilation = 1; // # Dilates time. Higher val = slower time.
-let colorHeightAdjustment = -50; // # Higher means lower threshold for color.
+let colorHeightAdjustment = 50; // # Higher means lower threshold for color.
 
 
 
@@ -19,9 +19,15 @@ function setup() {
 function draw() {
   background(50);
 
+  // Data generation for tests
+  // if (random(10) < 1) {
+  //   data.push(random(-100,0));
+  // }
+
+  
   // # Do time-dilation (slower/faster value read)
   //let dilated = timeDilation(dilation, data)
-  let dilated = data.pop();
+  let dilated = timeDilation(dilation, data);
   
   // # Do potential lerping
   if (dilated) {
@@ -62,18 +68,25 @@ function renderWave(simiVal){
   let colorPoint = new ColorPoint(simiVal);
   waveHistory.unshift(colorPoint);
  
-  // # Draw
+  
   for (let i = 0; i < waveHistory.length; i++) {
     // # Adjust xPosition to account for value scaling (time dilation).
-    let xPos = (waveHistory.length - i) / dilation
+    
     if (i > 0) { // # For the purpose of connecting vertices.
-      beginShape();
-      waveHistory[i].render(xPos, colorHeightAdjustment)
-      // # Connect with last to create shape.
-      waveHistory[i-1].render(xPos, colorHeightAdjustment)
-      endShape()
+
+      let xPosA = (waveHistory.length - i) / dilation
+      let yPosA = waveHistory[i].value;
+      
+      let xPosB = (waveHistory.length - i - 1) / dilation
+      let yPosB = waveHistory[i-1].value;
+      
+      let currentColor = waveHistory[i].getColor(colorHeightAdjustment)
+      
+      stroke(currentColor)
+      line(xPosB, yPosB, xPosA, yPosA)
     }
   }
+  
   // # Clear old.
   if (waveHistory.length / dilation > width - width/10) {
     waveHistory.pop()
@@ -87,9 +100,8 @@ class ColorPoint{
   constructor(value) {
     this.value = value
   }  
-  render(xPos, colorAdjustment){
-    stroke(colorAdjustment-this.value, 0, colorAdjustment-this.value)
-    vertex(xPos, this.value);
+  getColor(colorAdjustment) {
+  return [colorAdjustment-this.value, 0, colorAdjustment-this.value]
   }
 }
 
