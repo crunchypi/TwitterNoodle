db1aa44151e3026d24651be86662286a390b3262
crunchypi <alekscrank@gmail.com>
2020-03-04 20:07:12 +0100

    Added docstrings

--- packages/cleaning/data_object.py
+++ packages/cleaning/data_object.py
@@ -1,7 +1,15 @@
 class DataObj():
 
-    alphatags = [] # // @ consider removing..
+    """ This class is meant to be a container of 
+        tweet data for the TwitterNoodle project.
+        It is passed along most of the system.
+    """
+
     def __init__(self):
+        """ Initialises an emty DataObj with
+            some useful fields. Values are 
+            either None, False or [].
+        """
         self.unique_id = None
         self.name = None
         
@@ -13,4 +21,5 @@ class DataObj():
         self.alphatags = []
         self.valid_sentiment_range = False
 
-        self.siminet = [] # // compressed similarity net as in ProcessSimilarity class
+        # // Note: siminet is created in packages.similarity.process_tools.
+        self.siminet = []
