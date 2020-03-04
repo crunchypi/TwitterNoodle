95e60958c80f153ead29969b307b9be1c3eb1db8
crunchypi <alekscrank@gmail.com>
2020-03-04 19:53:02 +0100

    Added docstrings and typehints

--- packages/cleaning/custom_stopwords.py
+++ packages/cleaning/custom_stopwords.py
@@ -1,10 +1,19 @@
 from nltk.corpus import stopwords 
 
+""" A minimalistic module which combines custom 
+    stopwords with nltk.corpus.stopwords
+"""
+
 import_file = "packages/cleaning/custom_stopwords_list.txt"
 
-collection = []
+__collection = []
 
-def file_to_list(path, out_list):
+def __file_to_list(path:str, out_list:list) -> None:
+    """ Simply open a file and import words
+        listed (one word per line), strips 
+        new-line character and puts content
+        into a 'out_list'.
+    """
     with open(path, "r") as f:
         content = f.readline()
         while content:
@@ -13,9 +22,13 @@ def file_to_list(path, out_list):
             content = f.readline()
 
 
-def main():
-    file_to_list(import_file, collection)
+def main() -> set:
+    """ Combines custom stop-words with
+        stopwords from nltk.corpus.stopwords,
+        before returning a set (no duplicates).
+    """
+    __file_to_list(import_file, __collection)
     stop_words = stopwords.words('english')
-    collection.extend(stop_words)
-    return set(collection)
+    __collection.extend(stop_words)
+    return set(__collection)
 
