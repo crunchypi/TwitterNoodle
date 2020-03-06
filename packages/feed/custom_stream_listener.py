edfef791aca6aa117915b9012522915c33398994
crunchypi <alekscrank@gmail.com>
2020-03-06 15:01:31 +0100

    Added docstrings

--- packages/feed/custom_stream_listener.py
+++ packages/feed/custom_stream_listener.py
@@ -1,20 +1,37 @@
 from tweepy import StreamListener 
 
 
-
 class CustomStreamListener(StreamListener):
 
-    def __init__(self, _destination, _stream_toggle, _warn_verbosity):
+    """ This is a subclass of a tweepy StreamListener
+        which is a bare minimum for handing the listener.
+        See tweepy documentation for more info.
+    """
+
+    def __init__(self, _destination:list, _stream_toggle:bool, _warn_verbosity:bool):
+        """ Initialise with:
+                - Bare-minimum requirements (call to super).
+                - Conditional printout of warnings (_warn_verbosity).
+                - A list where new tweets will go (_destination).
+        """
         super(CustomStreamListener,self).__init__()
         self.destination = _destination
         self.stream_toggle = _stream_toggle
         self.warn_verbosity = _warn_verbosity
 
     def on_status(self, status):
+        """ Tries to add new tweets to self.destination.
+            This can be disabled with self.stream_toggle=False.
+        """
         self.destination.append(status) if self.stream_toggle else self.out_warn("Stream OFF")
 
     def on_error(self, status_code):
-        # // AA: 420 is the error code when a rate limit is reached
+        """ Tweepy error callback, only implementation is
+            for rate limits, which automatically disables 
+            streaming. Other warnings are printed out with
+            conditional printing (self.out_warn).
+        """
+        # // 420 is the error code when a rate limit is reached
         if status_code == 420: 
             self.out_warn("Recieved rate limit warning, stopping stream")
             self.stream_toggle = False
@@ -23,7 +40,8 @@ class CustomStreamListener(StreamListener):
             self.out_warn(f"misc error: {status_code}")
 
     def out_warn(self, msg):
-        print(msg) if not self.warn_verbosity else ...
+        " Conditional printout "
+        if self.warn_verbosity: print(msg)
 
 
 
--- packages/feed/tweet_feed.py
+++ packages/feed/tweet_feed.py
@@ -5,29 +5,46 @@ import credentials
 
 class Feed():
 
+    """ This class is responsible for getting tweets, either
+        from tweepy and CustomStreamListener
+            (packages.feed.custom_stream_listener)
+        .. or from a dataset already stored on the machine,
+        which should be a pickle with a list of tweepy
+        tweet objects (Note: compressed pickles is not supported).
+    """
+
     def __init__(self):
-            self.auth = credentials.auth
-            self.api = tweepy.API(self.auth)
-            self.api.wait_on_rate_limit = True
-            self.api.wait_on_rate_limit_notify = True
+        """ Sets up properties for live listener (tweepy),
+            requires relevant credentials to be set in the 
+            root folder (credentials.py).
+        """
+        self.auth = credentials.auth
+        self.api = tweepy.API(self.auth)
+        self.api.wait_on_rate_limit = True
+        self.api.wait_on_rate_limit_notify = True
 
     def live_get_listener(self, queue_stream):
+        " Sets up CustomStramListener returns it. "
         listener = CSL(_destination = queue_stream,
                                     _stream_toggle = True,
                                         _warn_verbosity = True)
         return listener
 
     def live_get_streamer(self, listener, track):
+        " Sets up tweepy.Stream and returns it. "
         stream = tweepy.Stream(auth = self.auth, listener=listener)
         stream.filter(track=track, languages=["en"], is_async= True)
         return stream
 
-    # returns a chunk list, should consider using a generator
     def disk_get_tweet_queue(self, file_path):
-        try:
-            pickle_in = open(file_path, "rb")
-            data = pickle.load(pickle_in)
-            pickle_in.close()
-            return data
-        except:
-            print("warn: could not get tweet queue from file_path")
+        """ Returns a pickled dataset. should be list of tweepy tweets. 
+            Does it in one single batch.
+
+            Failure is allowed to crash, so incorrect file_path
+            is not the responsebility of this method.
+        """
+        pickle_in = open(file_path, "rb")
+        data = pickle.load(pickle_in)
+        pickle_in.close()
+        return data
+
